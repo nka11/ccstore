4ddd670e8231cb5491b2c49b4ed1eef4d3c69381
Yohyo <Yohyo>
2016-05-07 00:43:37 +0200

    Page inscription (non fonctionnelle). MAJ index

--- index.php
+++ index.php
@@ -9,18 +9,20 @@ if ($path == "" || $path == null || $path == "/index.php") {
 
 require 'vendor/autoload.php';
 
-require_once 'controller/PagesController.php';
 require_once 'controller/StoreController.php';
 require_once 'controller/ContactController.php';
+require_once 'controller/InscriptionController.php';
 require_once 'controller/DebugController.php';
 require_once 'controller/ErrorController.php';
 require_once 'controller/UserController.php';
+require_once 'controller/PagesController.php';
 
 use Pux\Executor;
 
 $mux = new Pux\Mux;
 $mux->mount("/store", new StoreController());
 $mux->mount("/contact", new ContactController());
+$mux->mount("/inscription", new InscriptionController());
 $mux->mount("/debug", new DebugController());
 $mux->mount("/error", new ErrorController());
 $mux->mount("/user", new UserController());
