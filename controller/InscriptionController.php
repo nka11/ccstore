0d09bc35c3688a8efcb6a6108cef6a51c76982ce
Yohyo <Yohyo>
2016-05-09 13:54:41 +0200

    templating : ajout form recherche dans store_sidebar. Début de codage fonctionnalité inscription

--- controller/InscriptionController.php
+++ controller/InscriptionController.php
@@ -13,4 +13,28 @@ class InscriptionController extends AbstractController {
     $produits = $pdao->getProduits();
     return parent::render('inscription.html', array("products" => $produits));
   }
+	/**
+	 * Post method
+	 * 
+	 * @Route("/post")
+	 */
+	/* EN REDACTION
+	function postAction(){
+		 $email= null;
+		 $password = null;
+		 $pwconfirm = null;
+		 $nom = null;
+		 $prenom = null;
+		 $adress = null;
+		 $code = null;
+		 $town = null;
+		 $phonenumber = null;
+		 if (array_key_exists('HTTP_CONTENT_TYPE',$_SERVER)
+		  && $_SERVER['HTTP_CONTENT_TYPE'] == "application/json") {
+		  $postRequest = json_decode(stream_get_contents(STDIN));
+		  $email = $postRequest->email;
+		  $password = $postRequest->password;
+		}
+	 }
+	 */
 }
