cf988f8b824fcf49d15a474e46249b4d32cd9ee5
Yohyo <Yohyo>
2016-05-14 17:43:57 +0200

    fix ProductDAOTest

--- tests/ProductDAOTest.php
+++ tests/ProductDAOTest.php
@@ -13,6 +13,43 @@ class ProductDAOTest extends PHPUnit_Framework_TestCase
       $this->assertInternalType('string',$product->title());
     }
   }
+  public function testcreateProduct(){
+	  $proData = new Product(array(
+		"title" => "Title test product",
+		"ref" => "Ref Product Test",
+		"price" => 5.00,
+		"tva" => 5.5,
+		"description" => "Description product test",
+		"img" => "img/path.test",
+	  ));
+	$pdao = new ProductDAO();
+	
+	$product = $pdao->createProduct($proData);
+	$this->assertNotInternalType('boolean', $product);
+	$this->assertInternalType('int', $product->id());
+	$this->assertInternalType('string', $product->title()); 
+	$this->assertInternalType('string', $product->ref());
+	$this->assertInternalType('float', $product->price());
+	$this->assertInternalType('float', $product->tva());
+	$this->assertInternalType('string', $product->description());
+	$this->assertInternalType('string', $product->img());
+	
+	$loadedproduct = $prodao->getProductById($product->id());
+	$this->assertNotInternalType('boolean', $loadedproduct);
+	$this->assertEquals($product->id(), $loadedproduct->id());
+	$this->assertEquals($product->title(), $loadedproduct->title());
+	$this->assertEquals($product->ref(), $loadedproduct->ref());
+	$this->assertEquals($product->price(), $loadedproduct->price());
+	$this->assertEquals($product->tva(), $copycustomer->tva());
+	$this->assertEquals($product->description(), $copycustomer->description());
+	$this->assertEquals($product->img(), $copycustomer->img());
+	  
+	$delpro = $pdao->deleteProduct($product);  // delete product
+	$this->assertInternalType('boolean', $delpro);  // confirm deletion
+	$this->assertTrue($delpro);  // confirm deletion
+  }
+  }
+  
   public function testGetProductsByCategory() {
     $pdao = new ProductDAO();
     $products = $pdao->getProductsByCategory(1);
