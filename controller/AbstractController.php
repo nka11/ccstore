061baf71a6b58b853b27b818afab2584dfea52ce
Court Circuit <contact@courtcircuit.bio>
2019-07-29 15:08:21 +0200

    backup of 201908291508

--- controller/AbstractController.php
+++ controller/AbstractController.php
@@ -60,8 +60,6 @@ class AbstractController extends \Pux\Controller {
 		$this->session['basket_amount']+= number_format($line['value'],2);
 	}
 	$_SESSION = $this->session;
-	echo "<br/><br/><br/>";
-	print_r($_SESSION);
 	$_SESSION['temp_request'] = (!$temp_request) // asked in pw forgotten
 	? null
 	: $temp_request;
